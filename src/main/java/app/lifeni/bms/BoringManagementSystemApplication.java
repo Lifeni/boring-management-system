b844921842f5c8d5262faf2c4a17f08cc606d6f4
梁峰宁 <liangfengning@foxmail.com>
2021-06-21 20:16:54 +0800

    第一次 Commit

---
+++ src/main/java/app/lifeni/bms/BoringManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package app.lifeni.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoringManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoringManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/app/lifeni/bms/BoringManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package app.lifeni.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoringManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
